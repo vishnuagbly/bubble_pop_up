a79fea4a992db8e4cdad142caaf70e212ca0c31e
Vishnu Agarwal <vishnuagbly@gmail.com>
2024-01-31 23:17:51 +0530

    initial commit

---
+++ google_sign_in_all_platforms/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import google_sign_in_all_platforms
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GoogleSignInAllPlatformsPlugin.register(with: registry.registrar(forPlugin: "GoogleSignInAllPlatformsPlugin"))
+}
---
+++ google_sign_in_all_platforms/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <google_sign_in_all_platforms/google_sign_in_all_platforms_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  GoogleSignInAllPlatformsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GoogleSignInAllPlatformsPluginCApi"));
+}
